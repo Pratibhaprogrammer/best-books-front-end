7a830bbb8af835f36cfb9e6b351b0bed507a18e5
Jacob Holmer <yacub90@gmail.com>
2021-03-30 18:20:41 -0700

    debugging

--- src/App.js
+++ src/App.js
@@ -9,69 +9,69 @@ import {
 } from "react-router-dom";
 import { withAuth0 } from '@auth0/auth0-react';
 import MyFavoriteBooks from './MyFavoriteBooks';
-import Profile from'./Profile';
+import Profile from './Profile';
 import Login from './Login';
 import Books from './BestBooks';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-      this.state = {
-        books: [],
-        email: ''
-      }
-  }
-
-  updateEmail = (userEmail) => this.setState({ email: userEmail});
-
-  getBooks = async (e) => {
-    e.preventDefault();
-    console.log(this.state.email);
-    try{
-      const SERVER = 'http://localhost:3001';
-      const books = await axios.get(`${SERVER}/books`, {params: {email: this.state.email}});
-
-      console.log(this.email);
-
-      this.setState({ books: books.data})
-    }catch(error){
-      console.error(error);
+    this.state = {
+      books: [],
+      email: ''
     }
   }
 
+
+
+  // getBooks = async (e) => {
+  //   e.preventDefault();
+  //   console.log(this.state.email);
+  //   try{
+  //     const SERVER = 'http://localhost:3001';
+  //     const books = await axios.get(`${SERVER}/books`, {params: {email: this.state.email}});
+
+  //     console.log(this.email);
+
+  //     this.setState({ books: books.data})
+  //   }catch(error){
+  //     console.error(error);
+  //   }
+
   render() {
-    console.log('app', this.props)
+    console.log('app', this.props);
     return (
       <>
         <Router>
           {/* <IsLoadingAndError> */}
-            <Header 
+          <Header
             logout={this.props.auth0.isAuthenticated}
-            />
-            <Switch>
-              <Route exact path="/">
-                <Login />
-                {this.props.auth0.isAuthenticated &&
-                  <>
-                    <MyFavoriteBooks />
-                    <Books books = {this.state.books}/>
-                  </>
-                }
-              </Route>
-              <Route exact path="/Profile">
-                {this.props.auth0.isAuthenticated &&
-                  <Profile />
-                }
-              </Route>
-            </Switch>
-            <Footer />
+          />
+          <Switch>
+            <Route exact path="/">
+              <Login />
+              {this.props.auth0.isAuthenticated &&
+                <>
+                  <MyFavoriteBooks />
+                  <Books books={this.state.books} />
+                </>
+              }
+            </Route>
+            <Route exact path="/Profile">
+              {this.props.auth0.isAuthenticated &&
+                <Profile />
+              }
+            </Route>
+          </Switch>
+          <Footer />
           {/* </IsLoadingAndError> */}
         </Router>
       </>
     )
   }
 }
+
 
 export default withAuth0(App);
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
 
-class Books extends React.Component{
-  render(){
-    return(
-      this.props.books.map((books, i)=>(
-        <div key= {i}>
-          {books.name}
-          {books.description}
-          {books.status}
-        </div>
-      ))
-      )
+class Books extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      books: [],
+      email:''
     }
   }
-export default Books;
+  componentDidMount = async () => {
+    try {
+      console.log('email', this.props.auth0.user.email);
+      const SERVER = 'http://localhost:3002';
+      const books = await axios.get(`${SERVER}/books`, { params: { email: this.props.auth0.user.email } });
+      console.log(books);
+      console.log(this.email);
+  
+      this.setState({ books: books.data })
+    } catch (error) {
+      console.error(error);
+    }
+    // updateEmail = (userEmail) => this.setState({ email: userEmail });
+  }
+  render() {
+    return (
+      <>
+      <h2>here are your books</h2>
+        {this.props.books.map((books, i) => (
+          <div key={i}>
+            {books.name}
+            {books.description}
+            {books.status}
+          </div>
+        ))}
+      </>
+      )
+    }
+}
+export default withAuth0(Books);
